1e9378feb5906a834758914ee475515dc57dc2f3
PamndulaPPM <pandappm6@gmail.com>
2021-11-28 18:39:18 +0530

    create mock heroes and displaying

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,19 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 10001, name: 'Hulk' },
+  { id: 10002, name: 'Iron Man' },
+  { id: 10003, name: 'Captain America' },
+  { id: 10004, name: 'Spider Man' },
+  { id: 10005, name: 'Super Man' },
+  { id: 10006, name: 'Bat Man' },
+  { id: 10007, name: 'Falcon' },
+  { id: 10008, name: 'Wolverine' },
+  { id: 10009, name: 'Thor' },
+  { id: 10010, name: 'Black Panther' },
+  { id: 10011, name: 'Flash' },
+  { id: 10012, name: 'Doctor Strange' },
+  { id: 10013, name: 'Deadpool' },
+  { id: 10014, name: 'Iron Fist' },
+  { id: 10015, name: 'Groot' },
+];
