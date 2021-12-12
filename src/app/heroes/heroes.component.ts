8a1eb977065c1a2ba31314e7f7caba8066df0c28
PamndulaPPM <pandappm6@gmail.com>
2021-12-12 10:32:32 +0530

    display additional message when user click on a specific hero from the list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,12 +12,17 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
 
   selectedHero?: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {
+    this.getHeroes();
   }
 
-  constructor(private heroService: HeroService) {
-    this.getHeroes();
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   ngOnInit(): void {}
